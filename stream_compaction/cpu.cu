8997104341556dedf20252a11da9544bcb027040
Charles Wang <zwcharl@gmail.com>
2025-09-11 16:14:13 -0400

    run clang-format

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,95 +1,95 @@
-#include "cpu.h"
 #include "common.h"
+#include "cpu.h"
 
 #include <memory>
 
 namespace StreamCompaction {
-    namespace CPU {
-        using StreamCompaction::Common::PerformanceTimer;
+namespace CPU {
+using StreamCompaction::Common::PerformanceTimer;
 
-        namespace {
-          inline void scanImplementation(int n, int* odata, const int* idata) {
-            int currentSum = 0;
-            for (int i = 0; i < n; ++i) {
-              odata[i] = currentSum;
-              currentSum += idata[i];
-            }
-          }
-        }
-        
-        PerformanceTimer& timer()
-        {
-            static PerformanceTimer timer;
-            return timer;
-        }
-
-        /**
-         * CPU scan (prefix sum).
-         * For performance analysis, this is supposed to be a simple for loop.
-         * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function first.
-         */
-        void scan(int n, int *odata, const int *idata) {
-            timer().startCpuTimer();
-            
-            if (n <= 0) return;
-
-            scanImplementation(n, odata, idata);
-
-            timer().endCpuTimer();
-        }
-
-        /**
-         * CPU stream compaction without using the scan function.
-         *
-         * @returns the number of elements remaining after compaction.
-         */
-        int compactWithoutScan(int n, int *odata, const int *idata) {
-            timer().startCpuTimer();
-            
-            if (n <= 0) return 0;
-
-            int outputIndex = 0;
-            for (int inputIndex = 0; inputIndex < n; ++inputIndex) {
-              int element = idata[inputIndex];
-
-              if (element > 0) {
-                odata[outputIndex] = element;
-                outputIndex++;
-              }
-            }
-
-            timer().endCpuTimer();
-
-            return outputIndex;
-        }
-
-        /**
-         * CPU stream compaction using scan and scatter, like the parallel version.
-         *
-         * @returns the number of elements remaining after compaction.
-         */
-        int compactWithScan(int n, int *odata, const int *idata) {
-            timer().startCpuTimer();
-
-            if (n <= 0) return 0;
-            
-            std::unique_ptr<int[]> valid = std::make_unique<int[]>(n);
-            for (int i = 0; i < n; ++i) {
-              valid[i] = idata[i] > 0 ? 1 : 0;
-            }
-
-            std::unique_ptr<int[]> scanResult = std::make_unique<int[]>(n);
-            scanImplementation(n, scanResult.get(), valid.get());
-
-            for (int i = 0; i < n; ++i) {
-              if (valid[i] > 0) {
-                odata[scanResult[i]] = idata[i];
-              }
-            }
-
-            timer().endCpuTimer();
-
-            return scanResult[n - 1];
-        }
-    }
+namespace {
+inline void scanImplementation(int n, int* odata, const int* idata) {
+  int currentSum = 0;
+  for (int i = 0; i < n; ++i) {
+    odata[i] = currentSum;
+    currentSum += idata[i];
+  }
 }
+}  // namespace
+
+PerformanceTimer& timer() {
+  static PerformanceTimer timer;
+  return timer;
+}
+
+/**
+ * CPU scan (prefix sum).
+ * For performance analysis, this is supposed to be a simple for loop.
+ * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function
+ * first.
+ */
+void scan(int n, int* odata, const int* idata) {
+  timer().startCpuTimer();
+
+  if (n <= 0) return;
+
+  scanImplementation(n, odata, idata);
+
+  timer().endCpuTimer();
+}
+
+/**
+ * CPU stream compaction without using the scan function.
+ *
+ * @returns the number of elements remaining after compaction.
+ */
+int compactWithoutScan(int n, int* odata, const int* idata) {
+  timer().startCpuTimer();
+
+  if (n <= 0) return 0;
+
+  int outputIndex = 0;
+  for (int inputIndex = 0; inputIndex < n; ++inputIndex) {
+    int element = idata[inputIndex];
+
+    if (element > 0) {
+      odata[outputIndex] = element;
+      outputIndex++;
+    }
+  }
+
+  timer().endCpuTimer();
+
+  return outputIndex;
+}
+
+/**
+ * CPU stream compaction using scan and scatter, like the parallel version.
+ *
+ * @returns the number of elements remaining after compaction.
+ */
+int compactWithScan(int n, int* odata, const int* idata) {
+  timer().startCpuTimer();
+
+  if (n <= 0) return 0;
+
+  std::unique_ptr<int[]> valid = std::make_unique<int[]>(n);
+  for (int i = 0; i < n; ++i) {
+    valid[i] = idata[i] > 0 ? 1 : 0;
+  }
+
+  std::unique_ptr<int[]> scanResult = std::make_unique<int[]>(n);
+  scanImplementation(n, scanResult.get(), valid.get());
+
+  for (int i = 0; i < n; ++i) {
+    if (valid[i] > 0) {
+      odata[scanResult[i]] = idata[i];
+    }
+  }
+
+  timer().endCpuTimer();
+
+  return scanResult[n - 1];
+}
+}  // namespace CPU
+}  // namespace StreamCompaction
--- stream_compaction/thrust.cu
+++ stream_compaction/thrust.cu
@@ -7,22 +7,21 @@
 #include "thrust.h"
 
 namespace StreamCompaction {
-    namespace Thrust {
-        using StreamCompaction::Common::PerformanceTimer;
-        PerformanceTimer& timer()
-        {
-            static PerformanceTimer timer;
-            return timer;
-        }
-        /**
-         * Performs prefix-sum (aka scan) on idata, storing the result into odata.
-         */
-        void scan(int n, int *odata, const int *idata) {
-            timer().startGpuTimer();
-            // TODO use `thrust::exclusive_scan`
-            // example: for device_vectors dv_in and dv_out:
-            // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
-            timer().endGpuTimer();
-        }
-    }
+namespace Thrust {
+using StreamCompaction::Common::PerformanceTimer;
+PerformanceTimer& timer() {
+  static PerformanceTimer timer;
+  return timer;
 }
+/**
+ * Performs prefix-sum (aka scan) on idata, storing the result into odata.
+ */
+void scan(int n, int* odata, const int* idata) {
+  timer().startGpuTimer();
+  // TODO use `thrust::exclusive_scan`
+  // example: for device_vectors dv_in and dv_out:
+  // thrust::exclusive_scan(dv_in.begin(), dv_in.end(), dv_out.begin());
+  timer().endGpuTimer();
+}
+}  // namespace Thrust
+}  // namespace StreamCompaction
